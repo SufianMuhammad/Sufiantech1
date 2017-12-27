5c9f3c1137716ea742de9801446e576d4ff56182
SufianMuhammad <rumitajmahal@gmail.com>
2017-12-27 00:42:38 -0500

    I correction some code

---
+++ src/my_thirdprogram/HelloDosto.java
@@ -0,0 +1,11 @@
+package my_thirdprogram;
+
+public class HelloDosto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello dosto");
+
+	}
+
+}
