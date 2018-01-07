a2908b967391d0ed920b42e1fcf77753c5e7a284
SufianMuhammad <rumitajmahal@gmail.com>
2018-01-07 14:55:52 -0500

    i copy some program

---
+++ src/my_thirdprogram/if_else.java
@@ -0,0 +1,20 @@
+package my_thirdprogram;
+
+public class if_else {
+
+	public static void main(String[] args) {
+		int age = 15;
+
+		if (age>=18) {
+			System.out.println("You can  open an account");
+		}else {if (age<18) {
+
+			System.out.println("You can not open an account"); 
+		}
+
+		}
+
+
+	}
+
+}
